f179afec498b301dbd610a0f96981a9cd64729ec
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-06-21 16:38:38 +0600

    Add project files.

---
+++ CRUDAPP.ENTITIES/Models/EmployeeModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDAPP.ENTITIES.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long SysId { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+        public int Age { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
